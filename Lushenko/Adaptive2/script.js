ce1215eaf306478e96ee0186f7d75e527d712131
yuriksensej <yuraminchenko@gmail.com>
2021-02-20 18:22:27 +0200

    adaptive slider done

---
+++ Lushenko/Adaptive2/script.js
@@ -0,0 +1,33 @@
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider-line');
+let count = 0;
+let width;
+function init() {
+  width = document.querySelector('.slider').offsetWidth;
+  sliderLine.style.width = width * images.length + 'px';
+  images.forEach((item) => {
+    item.style.width = width + 'px';
+
+    item.style.height = 'auto';
+  });
+  rollSlider();
+}
+window.addEventListener('resize', init);
+init();
+document.querySelector('.slider-next').addEventListener('click', () => {
+  count += -1;
+  if (count < -images.length + 1) {
+    count = 0;
+  }
+  rollSlider();
+});
+document.querySelector('.slider-prev').addEventListener('click', () => {
+  count++;
+  if (count > 0) {
+    count = -images.length + 1;
+  }
+  rollSlider();
+});
+function rollSlider() {
+  sliderLine.style.transform = 'translate(' + count * width + 'px)';
+}
