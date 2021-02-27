9b70b1bbcb7bc5280947332327c882fc17365200
yuriksensej <yuraminchenko@gmail.com>
2021-02-27 20:28:36 +0200

    test 27.02.21

--- test/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$array = array("foo", "bar", "hallo", "world");
-
-
-foreach ($array as $key => $value) {
-   // var_dump($key . ':' . $value . '<br>');
-   echo $key . ':' . $value . '<br>';
-}
-// var_dump();
